fdadf03812b629e5fb33e3acfe72130e4e2804d8
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-04-02 22:34:51 +0545

    JS FProject

--- count.js
+++ count.js
@@ -1,4 +1,5 @@
 let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
 count=0
 
 function present() {
@@ -6,5 +7,9 @@ count=count+1
 countEl.innerText=count
 }
 function save() {
+    countStr=" - " + count
+    saveEl.textContent += countStr
+    countEl.textContent=0
+    count=0
     console.log(count)
 }
