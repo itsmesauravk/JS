8620f67e4d5b11644692ab0c2856397620a5bec2
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-06-01 15:05:13 +0545

    added

---
+++ weatherApp/script.js
@@ -0,0 +1,16 @@
+const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
