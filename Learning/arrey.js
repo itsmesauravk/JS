970a373df9f068c8947f0ae0f2597408ad245f75
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-06-07 21:38:03 +0545

    new learned

---
+++ Learning/arrey.js
@@ -0,0 +1,15 @@
+// about array
+// basic array
+// list in python, array in JS
+let fruits = ["apple","mango","banana"]
+// console.log(typeof(fruits))   so the type of array is object
+// console.log(fruits[0])  follow index method
+fruits[2] = "leechi"  //changes or update the second index with new one
+//multi dimensional array
+let numbrs = [[1,2,4],[[1,5,6],'x'],'a', 0]
+rslt = numbrs[1][1] //to logout the value 'x'
+rslt = numbrs[1][0][1]  //to logout the value 5
+
+let list = ['app', 'website']
+list.push(['game','music'])  //it add every thing inside () so result will be [ 'app', 'website', [ 'game', 'music' ] ]
+
