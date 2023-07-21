77bfa1e6c13a09e190e561a8d5437dd9d103da90
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-07-21 22:24:04 +0545

    Add files via upload

---
+++ random_simp_pass/script.js
@@ -0,0 +1,19 @@
+const start = document.getElementById("start")
+const dispResult = document.getElementById("result")
+function password(len){
+    let pass = "qwertyuiopasdfghjklzxcvbnm.()@#$%!"
+    disp = ''
+    i = 0
+    while(i<len){
+        disp += pass[Math.floor(Math.random()*pass.length)]
+        i ++
+    }
+    dispResult.textContent = disp
+}
+start.addEventListener("click",()=>{
+    let long = document.querySelector("#passlength").value
+    console.log(long)
+    password(long)
+})
+
+
