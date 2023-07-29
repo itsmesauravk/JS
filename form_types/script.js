f89b83ef9a16d32e1e095825cf9d17fea4ceb8fe
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-07-30 01:05:33 +0545

    Add files via upload

---
+++ form_types/script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('btn')
+const disp = document.querySelector(".result")
+
+const listy = []
+const store = () =>{
+    const vals = document.getElementById('inputv')
+    listy.push(vals.value)
+    // let output = listy.join(", ")
+
+    //creating unordred list
+    let ulEle = document.createElement("ol")
+    for(i=0;i<listy.length;i++){
+        let lstEle = document.createElement("li")
+        lstEle.textContent = listy[i]
+        ulEle.appendChild(lstEle)
+    }
+    disp.innerHTML=''
+    disp.appendChild(ulEle)
+    
+    vals.value = ''
+}
+
+btn.addEventListener('click',()=>{
+    store()
+})
