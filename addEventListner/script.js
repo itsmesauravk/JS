a9154c71fad7abcb8a23324f333c4af2664c381d
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-08-27 22:43:42 +0545

    Add files via upload

---
+++ addEventListner/script.js
@@ -0,0 +1,10 @@
+let grandParent = document.querySelector('.grandparent')
+let parent = document.querySelector('.parent')
+let child = document.querySelector('.child')
+
+grandParent.addEventListener("click", e => {
+    console.log("hi")
+})
+
+// in the above 
+// documentname.addEventListner("name of function or can search on google", program to be executed or function)
