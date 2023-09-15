a70ec1a99a1a82e93f464a224fb92d6952b09571
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-09-15 22:13:43 +0545

    Add files via upload

---
+++ revision/script.js
@@ -0,0 +1,17 @@
+console.log("Doing good");
+
+// let fruit = prompt("which is your fav fruit ?");
+// let check = confirm("Is your fav fruit is " + fruit + " ?");
+
+// if (check) {
+//   alert(fruit + " is my favorite too ! ");
+// } else {
+//   alert(fruit + " is not my favourite too!.");
+// }
+
+let trying = function () {
+  let result = 10 + 2;
+  return result;
+};
+trying();
+console.log(trying());
